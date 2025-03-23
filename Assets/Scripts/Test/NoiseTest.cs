cf65410eec0397d19b06d3cde941158001a6d1eb
YB970902 <78996787+YB970902@users.noreply.github.com>
2025-03-23 15:31:01 +0900

    여러개의 서브 메시 테스트 추가

--- Assets/Scripts/Test/NoiseTest.cs
+++ Assets/Scripts/Test/NoiseTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using Bean.MC;
 using Bean.Noise;
 using Define;
@@ -19,18 +20,29 @@ public class NoiseTest : MonoBehaviour
 
     [SerializeField] private float isoLevel;
     [SerializeField] private float cubeSize;
+
+    [SerializeField] private float moveSpeed;
+    
+    [SerializeField] private int noiseGridScale;
+    
+    private Vector2 offset;
+    private Vector2 maxOffset;
+    
     private void Awake()
     {
         MarchingCubes.IsoLevel = isoLevel;
         MarchingCubes.CubeSize = cubeSize;
 
-        int gridXCount = Mathf.CeilToInt(axisXCount * cubeSize);
-        int gridZCount = Mathf.CeilToInt(axisZCount * cubeSize);
+        int gridXCount = Mathf.CeilToInt(axisXCount * cubeSize) * noiseGridScale;
+        int gridZCount = Mathf.CeilToInt(axisZCount * cubeSize) * noiseGridScale;
 
+        offset = Vector2.zero;
+        maxOffset = new Vector2(axisXCount * cubeSize * (noiseGridScale - 1), axisZCount * cubeSize * (noiseGridScale - 1));
+        
         noise = new PerlinNoise(gridXCount, gridZCount, 1f);
         noise.Init();
         generator = new CubeGenerator(axisXCount, axisZCount, axisYCount, prefab);
-        generator.Init();
+        generator.Init(CalcSubMeshIndex);
 
         for (int x = 0; x < axisXCount; ++x)
         {
@@ -42,10 +54,35 @@ public class NoiseTest : MonoBehaviour
         
         generator.CalcMeshes();
     }
-    
+
+    private void FixedUpdate()
+    {
+        for (int x = 0; x < axisXCount; ++x)
+        {
+            for (int z = 0; z < axisZCount; ++z)
+            {
+                SetHeight(x, z);
+            }
+        }
+        
+        generator.CalcMeshes();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+        offset.x += h * (moveSpeed * Time.deltaTime);
+        offset.y += v * (moveSpeed * Time.deltaTime);
+        
+        offset.x = Mathf.Clamp(offset.x, 0f, maxOffset.x);
+        offset.y = Mathf.Clamp(offset.y, 0f, maxOffset.y);
+    }
+
     private void SetHeight(int x, int z)
     {
-        float height = noise.GetNoise(x * cubeSize, z * cubeSize);
+        float height = noise.GetNoise(x * cubeSize + offset.x, z * cubeSize + offset.y);
         // height가 -1 ~ 1 이므로, 1을 더해서 0 ~ 2 로 만든 뒤에 2로 나눠 0 ~ 1로 만든다.
         height = (height + 1f) * 0.5f;
         height *= axisYCount;
@@ -54,5 +91,12 @@ public class NoiseTest : MonoBehaviour
         {
             generator.SetScalar(x, z, y, Mathf.Clamp01(height - y));
         }
+    }
+    
+    private int CalcSubMeshIndex(Vector3 position)
+    {
+        float height = axisYCount * cubeSize * 0.55f;
+        if (position.y >= height) return 1;
+        return 0;
     }
 }
